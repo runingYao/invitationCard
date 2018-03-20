e73b584ef9d6559ab276c0f128503728ad5bdb7e
runingyao <lj11244817>
2018-03-20 10:35:55 +0800

    替换背景图

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var app = {engine: new Engine(), elements: [], texturePath: "img/sun_temple_stripe.jpg", tilesNum: 6};
+var app = {engine: new Engine(), elements: [], texturePath: "img/bg.jpg", tilesNum: 6};
 app.initPages = function (textures) {
 
-    var materials = [];
+/*    var materials = [];
 
     for (var i = 0; i < 6; i++) {
 
@@ -12,7 +12,19 @@ app.initPages = function (textures) {
     }
     var skyBox = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), new THREE.MeshFaceMaterial(materials));
     skyBox.applyMatrix(new THREE.Matrix4().makeScale(1, 1, -1));
-    this.engine.scene.add(skyBox);
+    this.engine.scene.add(skyBox);*/
+
+
+    var geometry = new THREE.SphereGeometry(500, 120, 40);
+    geometry.scale(-1, 1, 1);
+
+    var material = new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load(app.texturePath)
+    });
+
+    var mesh = new THREE.Mesh(geometry, material);
+
+    this.engine.scene.add(mesh);
 
     this.elements[0] = $('<button type="button" name = "forward">跳到下一个位置</button>')[0];
     this.elements[0].position = new THREE.Vector3(0, 0.3, -0.49);
@@ -65,22 +77,23 @@ app.loader = function () {
     var imageObj = new Image();
 
     imageObj.onload = function () {
-
-        var canvas, context;
-        var tileWidth = imageObj.height;
-
-        for (var i = 0; i < textures.length; i++) {
-
-            canvas = document.createElement('canvas');
-            context = canvas.getContext('2d');
-            canvas.height = tileWidth;
-            canvas.width = tileWidth;
-            context.drawImage(imageObj, tileWidth * i, 0, tileWidth, tileWidth, 0, 0, tileWidth, tileWidth);
-            textures[i].image = canvas
-            textures[i].needsUpdate = true;
-
-        }
         app.initPages(textures);
+
+        //var canvas, context;
+        //var tileWidth = imageObj.height;
+        //
+        //for (var i = 0; i < textures.length; i++) {
+        //
+        //    canvas = document.createElement('canvas');
+        //    context = canvas.getContext('2d');
+        //    canvas.height = tileWidth;
+        //    canvas.width = tileWidth;
+        //    context.drawImage(imageObj, tileWidth * i, 0, tileWidth, tileWidth, 0, 0, tileWidth, tileWidth);
+        //    textures[i].image = canvas
+        //    textures[i].needsUpdate = true;
+        //
+        //}
+
 
     };
 
